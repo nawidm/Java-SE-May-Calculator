4a82cca63781293262546962d657a7ee53a6056d
nawidm <nawid_m@hotmail.co.uk>
2018-07-11 11:45:03 +0100

    subtract implemented

---
+++ src/main/java/com/qa/calculator/Operation.java
@@ -0,0 +1,5 @@
+package com.qa.calculator;
+
+public interface Operation {
+	public double doOperation(double num1, double num2);
+}
---
+++ src/main/java/com/qa/calculator/Subtract.java
@@ -0,0 +1,8 @@
+package com.qa.calculator;
+
+public class Subtract implements Operation{
+
+	public double doOperation(double num1, double num2) {
+		return num1-num2;
+	}
+}
